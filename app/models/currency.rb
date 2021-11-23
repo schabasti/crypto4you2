282c2b36c9cfd31cadb6f1769b03fc504a2719ce
Sebastian Burkhardt <ga29rox@mytum.de>
2021-11-23 17:08:47 +0100

    added dependencies in currency

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,6 +1,9 @@
 class Currency < ApplicationRecord
   has_many :user_currencies
-  has_many :trades
+  has_many :start_currencies_trades, class_name: "Trade",
+    foreign_key: :start_currency_id
+  has_many :end_currencies_trades, class_name: "Trade",
+    foreign_key: :end_currency_id
 
   validates :name, presence: true
   validates :is_crypto, presence: true
