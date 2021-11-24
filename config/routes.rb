bb35935a00bf86b6508cceae140b01b5a37828c0
Finn Erdmann <finn.erdmanntco@gmail.com>
2021-11-24 17:51:26 +0100

    user list

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trades, only: %i[show index]
-  get 'my_dashboard', to: "profiles#my_dashboard"
+  resources :users, only: %i[index]
+  get 'profiles/my_dashboard'
 end
