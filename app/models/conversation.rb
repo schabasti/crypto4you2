cacc88edc1a87ba79c11b5f3efb71d017eb3aeba
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-25 11:41:25 +0100

    setup conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,10 @@ class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :user1, class_name: "User"
   belongs_to :user2, class_name: "User"
+
+  scope :between, -> (user1, user2) do
+    find_by("(conversations.user1_id = :user1_id AND conversations.user2_id = :user2_id)
+        OR (conversations.user1_id = :user2_id AND conversations.user2_id = :user1_id)",
+            { user1_id: user1.id, user2_id: user2.id })
+  end
 end
