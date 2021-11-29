0bea9eb60e80a649617cd1dbdd61343b678fc246
Sebastian Burkhardt <ga29rox@mytum.de>
2021-11-29 14:46:20 +0100

    pie chart implemented

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,42 @@
 class ProfilesController < ApplicationController
+  # const DATA_COUNT = 5
+  # const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 }
+
   def my_dashboard
     @trades = current_user.trades.order(created_at: :desc)
+    colors = ["#003F5C", "#EC6B56", "#64C2A6", "#58508D", "#EC6B56", "#BC5090", "#FFC154"]
+    labels = current_user.user_currencies.map do |coins|
+      coins.currency.name
+    end
+    datas = current_user.user_currencies.map do |coins|
+      coins.amount
+    end
     @chart_data = {
-      labels: ["Ethereum", "Bitcoin", "Solana", "Dogecoin", "Polkadot"],
+      labels: labels,
       datasets: [{
-        label: "My First Trades",
-        backgroundColor: "transparent",
-        borderColor: "#548E89",
-        data: [53, 68, 78, 56, 65]
+        label: 'My First dataset',
+        backgroundColor: colors,
+        borderColor: '#878B93',
+        data: datas
       }]
+    }
+    # @chart_options = {
+    #   type: 'pie',
+    #   data: data,
+    # }
+    @chart_options = {
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top'
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Pie Chart'
+          }
+        }
+      }
     }
   end
 
