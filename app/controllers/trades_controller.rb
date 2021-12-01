20fd6cd2ffd4e990ae0847e6086417db345793f8
Marcus von Wrede <m.vonwrede@gmx.de>
2021-12-01 16:09:57 +0100

    order trades

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,6 +1,6 @@
 class TradesController < ApplicationController
   def index
-    @trades = Trade.where(user: current_user)
+    @trades = Trade.where(user: current_user).order(date: :desc)
   end
 
   def show
