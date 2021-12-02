99d07c681f4ac0eea1607944de3ec99a72bdbe67
Sebastian Burkhardt <ga29rox@mytum.de>
2021-12-02 12:46:10 +0100

    changed size to fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,9 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-
+import Chart from "stimulus-chartjs"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+application.register("chart", Chart)
